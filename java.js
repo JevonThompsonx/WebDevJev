8a86df559b8dad81680c56d41ac1a3c5c088b1dd
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-03-15 14:12:31 -0400

    Updated styling for dark/light mode

---
+++ java.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1')
+const h2s = document.querySelectorAll('h2')
+const h3s = document.querySelectorAll('h3')
+const pTags = document.querySelectorAll('p')
+const links = document.querySelectorAll('a')
+const listItems = document.querySelectorAll('li')
+c
