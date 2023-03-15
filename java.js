32b468a139c7245d36a3b2a7dec8bef6eee46daa
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-03-15 19:59:46 -0400

    media queries complete except for contact

--- java.js
+++ java.js
@@ -4,4 +4,14 @@ const h3s = document.querySelectorAll('h3')
 const pTags = document.querySelectorAll('p')
 const links = document.querySelectorAll('a')
 const listItems = document.querySelectorAll('li')
-c
+const togglerIcon = document.querySelector('.navbar-toggler-icon')
+let iconNumber = 0
+togglerIcon.addEventListener('click', ()=> {
+    iconNumber++
+    if (iconNumber % 2 === 0) { 
+      togglerIcon.innerHTML =  '<i class="fa-sharp fa-solid fa-angle-down"></i>'
+    }
+    else {
+    togglerIcon.innerHTML = '<i class="fa-solid fa-angle-up"></i>'
+    }
+})
