98a0ba41da42fdaa55bd663cec47e96599dcdf68
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-03-16 20:07:34 -0400

    Notion notes added

--- java.js
+++ java.js
@@ -1,10 +1,3 @@
-const h1 = document.querySelector('h1')
-const h2s = document.querySelectorAll('h2')
-const h3s = document.querySelectorAll('h3')
-const pTags = document.querySelectorAll('p')
-const links = document.querySelectorAll('a')
-const listItems = document.querySelectorAll('li')
-const togglerIcon = document.querySelector('.navbar-toggler-icon')
 let iconNumber = 0
 togglerIcon.addEventListener('click', ()=> {
     iconNumber++
