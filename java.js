2e9157aa07be5f0277123fc409eb041d128cc04e
JevonThompsonx <104575457+JevonThompsonx@users.noreply.github.com>
2023-03-17 23:25:14 -0400

    Made light mode fancy

--- java.js
+++ java.js
@@ -9,3 +9,15 @@ togglerIcon.addEventListener('click', ()=> {
     togglerIcon.innerHTML = '<i class="fa-solid fa-angle-up"></i>'
     }
 })
+const welcomeImg = document.querySelector('#welcome-img')
+welcomeImg.addEventListener('load' , ()=> {
+if (window.matchMedia) {
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+       welcomeImg.src = 'images/Me-modified.webp'
+ 
+  } else {
+    welcomeImg.src = 'images/Me-lightMode-modified.webp'
+  }
+} else {
+}
+})
